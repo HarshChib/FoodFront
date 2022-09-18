94643ad75e9988f8b7b6cb29c9b84b000813bf2b
Sarthik <Sarthikkumar26@gmail.com>
2022-09-18 21:02:50 +0530

    first commit

--- FoodFront/src/app/app.module.ts
+++ FoodFront/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddMenuComponent } from './add-menu/add-menu.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+     FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
