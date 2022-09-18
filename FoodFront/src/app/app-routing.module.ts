903f02993a6fddb5db474be9f0ec46c0ab6d08fb
Harsh Chib <harshchib70@gmail.com>
2022-09-18 15:41:03 +0530

    2nd Commit

--- FoodFront/src/app/app-routing.module.ts
+++ FoodFront/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardsComponent } from './cards/cards.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { SettingsComponent } from './settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:LoginComponent},
+  {path:"login",component:LoginComponent},
+  {path:"register",component:RegistrationComponent},
+  {path:"dashboard",component:DashboardComponent,children:[
+    { path: 'cards', component: CardsComponent },
+     { path: 'settings', component: SettingsComponent }
+  ]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ FoodFront/src/app/cards/cards.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit ,Output, EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrls: ['./cards.component.css']
+})
+export class CardsComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
