8a8d754a0e9c225b1d769258d156a4b7df40754e
Sarthik <Sarthikkumar26@gmail.com>
2022-09-20 23:47:07 +0530

    form validations done

--- FoodFront/src/app/settings/settings.component.ts
+++ FoodFront/src/app/settings/settings.component.ts
@@ -12,20 +12,24 @@ export class SettingsComponent implements OnInit {
   data:any;
   constructor(private user:UserService,private route:Router) { }
   ngOnInit(): void {
-    this.user.getUser(localStorage.getItem('id')).subscribe((res)=>{
-        console.log(res);
-        this.data=res;
+  
+
+     this.user.getUser(localStorage.getItem('id')).subscribe((res)=>{
+         console.log(res);
+         this.data=res;
         console.log(this.data.name);
-      })
+       })
    }
-  updateUser(form:NgForm){
-    console.log(form.value);
+   onSubmit(form:NgForm){
+     console.log(form.value);
     this.user.updateUser(form.value,localStorage.getItem('id')).subscribe((res)=>{
-      console.log(res);
-    })
-    this.go();
-  }
-  go(){
-		this.route.navigate(['/dashboard/cards']); // navigate to other page
-	}
+       console.log(res);
+     })
+     this.go();
+   }
+   go(){
+	 	this.route.navigate(['/dashboard/cards']); // navigate to other page
+  
 }
+}
+
